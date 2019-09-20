42822d8b032b4c108ff25ccc93e6b957a036809c
bkdesktop <sd@noneyabuisiness>
2019-09-20 10:47:18 -0700

    HELLO KOGAN COMMIT

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
 
 from pidev.MixPanel import MixPanel
 from pidev.kivy.PassCodeScreen import PassCodeScreen
@@ -17,7 +20,7 @@ MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
 SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
 ADMIN_SCREEN_NAME = 'admin'
-
+BUTTON_SCREEN_NAME = 'button'
 
 class ProjectNameGUI(App):
     """
@@ -34,18 +37,38 @@ class ProjectNameGUI(App):
 
 Window.clearcolor = (1, 1, 1, 1)  # White
 
+class buttonScreen(Screen):
+    def __init__(self, **kwargs):
+        Builder.load_file('buttonScreen.kv')
+        super(buttonScreen, self).__init__(**kwargs)
+
+    def pressed(self):
+        SCREEN_MANAGER.current = 'main'
 
 class MainScreen(Screen):
+
+    counter = StringProperty("0")
+    toggle = ObjectProperty(False)
+
     """
     Class to handle the main screen and its associated touch events
     """
+    def pressed_counter(self):
+        tempCounter = int(self.counter)
+        tempCounter += 1
+        self.counter = str(tempCounter)
+
+    def pressed_motor_toggle(self):
+        self.toggle = not self.toggle
 
     def pressed(self):
         """
         Function called on button touch event for button with id: testButton
         :return: None
         """
-        PauseScreen.pause(pause_scene_name='pauseScene', transition_back_scene='main', text="Test", pause_duration=5)
+        #PauseScreen.pause(pause_scene_name='pauseScene', transition_back_scene='main', text="Test", pause_duration=5)
+
+        SCREEN_MANAGER.current = 'button'
 
     def admin_action(self):
         """
@@ -106,6 +129,7 @@ SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
 SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
 SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
 SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(buttonScreen(name=BUTTON_SCREEN_NAME))
 
 """
 MixPanel
